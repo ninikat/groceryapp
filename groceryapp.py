4d4129652aa08168925c4ac1f44b751617b948b8
Nydia Segura <nydia.segura@gmail.com>
2017-11-20 14:47:34 -0600

    with sams items

--- groceryapp.py
+++ groceryapp.py
@@ -40,12 +40,22 @@ fiesta.add_grocery_items(soda.name)
 fiesta.add_grocery_items(fish.name)
 fiesta.print_out()
 
-walmart = ShoppingList('Walamrt','random')
+walmart = ShoppingList('Walmart','random')
 walmart.add_grocery_items(paper.name)
 walmart.add_grocery_items(napkins.name)
 walmart.add_grocery_items(plate.name)
 walmart.add_grocery_items(chips.name)
 walmart.print_out()
+
+sams_club = ShoppingList('Sam\'s Club', 'random')
+sams_club.add_grocery_items(chicken.name)
+sams_club.add_grocery_items(beef.name)
+sams_club.add_grocery_items(eggs.name)
+sams_club.add_grocery_items(sugar.name)
+sams_club.add_grocery_items(salt.name)
+sams_club.add_grocery_items(pepper.name)
+sams_club.add_grocery_items(honey.name)
+sams_club.print_out()
 
 
 
